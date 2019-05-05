724185c2834b1d1135817b47da90e65817f4589d
Wesley Dashner <wdashner11@gmail.com>
2019-05-05 00:50:56 -0700

    implemented Tile class into Board class in order to give captured attribute

--- Color 20 Refactoring/Board.swift
+++ Color 20 Refactoring/Board.swift
@@ -9,8 +9,13 @@
 import Foundation
 import SpriteKit
 
+struct Tile {
+    var sprite: SKSpriteNode
+    var captured: Bool
+}
+
 class Board {
-    var tiles: Array<Array<SKSpriteNode>>
+    var tiles: Array<Array<Tile>>
     
     init(x: Int, y: Int, widthOfBoard: Int, gameScene: SKScene, centerX: Int = 0, centerY: Int = 0) {
         tiles = []
@@ -19,7 +24,7 @@ class Board {
         }
         for rowIndex in 0...(tiles.count - 1) {
             for _ in 1...x {
-                tiles[rowIndex].append(SKSpriteNode())
+                tiles[rowIndex].append(Tile(sprite: SKSpriteNode(), captured: false))
             }
         }
         randomizeBoard()
@@ -31,7 +36,7 @@ class Board {
         
         for row in tiles {
             for columnIndex in 0...(row.count - 1) {
-                row[columnIndex].color = colors.randomElement()!
+                row[columnIndex].sprite.color = colors.randomElement()!
             }
         }
     }
@@ -43,21 +48,25 @@ class Board {
         
         for rowIndex in 0...(tiles.count - 1) {
             for columnIndex in 0...(tiles[rowIndex].count - 1) {
-                tiles[rowIndex][columnIndex].size = CGSize(width: widthOfTile, height: widthOfTile)
+                tiles[rowIndex][columnIndex].sprite.size = CGSize(width: widthOfTile, height: widthOfTile)
                 if x % 2 == 0 {
-                    tiles[rowIndex][columnIndex].position.x = CGFloat(-widthOfTile * Double(x / 2 - (columnIndex + 1)) - widthOfTile / 2 + Double(centerX))
+                    tiles[rowIndex][columnIndex].sprite.position.x = CGFloat(-widthOfTile * Double(x / 2 - (columnIndex + 1)) - widthOfTile / 2 + Double(centerX))
                 }
                 else {
-                    tiles[rowIndex][columnIndex].position.x = CGFloat(-widthOfTile * Double(x / 2 - (columnIndex + 1)) - widthOfTile + Double(centerX))
+                    tiles[rowIndex][columnIndex].sprite.position.x = CGFloat(-widthOfTile * Double(x / 2 - (columnIndex + 1)) - widthOfTile + Double(centerX))
                 }
                 if y % 2 == 0 {
-                    tiles[rowIndex][columnIndex].position.y = CGFloat(widthOfTile * Double(y / 2 - (rowIndex + 1)) + widthOfTile / 2 + Double(centerY))
+                    tiles[rowIndex][columnIndex].sprite.position.y = CGFloat(widthOfTile * Double(y / 2 - (rowIndex + 1)) + widthOfTile / 2 + Double(centerY))
                 }
                 else {
-                    tiles[rowIndex][columnIndex].position.y = CGFloat(widthOfTile * Double(y / 2 - (rowIndex + 1)) + widthOfTile + Double(centerY))
+                    tiles[rowIndex][columnIndex].sprite.position.y = CGFloat(widthOfTile * Double(y / 2 - (rowIndex + 1)) + widthOfTile + Double(centerY))
                 }
-                gameScene.addChild(tiles[rowIndex][columnIndex])
+                gameScene.addChild(tiles[rowIndex][columnIndex].sprite)
             }
         }
+    }
+    
+    func doColor(color: UIColor) {
+        
     }
 }
