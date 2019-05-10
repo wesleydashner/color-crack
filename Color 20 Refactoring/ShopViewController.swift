4dc777d2ce402a192eed973b2c9e1e5a7b3f08cf
Wesley Dashner <wdashner11@gmail.com>
2019-05-10 15:20:28 -0600

    added upgrade button

--- Color 20 Refactoring/ShopViewController.swift
+++ Color 20 Refactoring/ShopViewController.swift
@@ -13,6 +13,8 @@ import GameplayKit
 class ShopViewController: UIViewController {
     
     let scene = SKScene(size: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+    let moneyLabel = SKLabelNode()
+    let upgradeButton = SKSpriteNode(imageNamed: "upgrade")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +24,28 @@ class ShopViewController: UIViewController {
         scene.scaleMode = .aspectFit
         view?.presentScene(scene)
         view?.ignoresSiblingOrder = true
+        
+        let money = UserDefaults.standard.integer(forKey: "money")
+        moneyLabel.text = "$\(money)"
+        moneyLabel.fontSize = 20
+        moneyLabel.fontColor = .yellow
+        moneyLabel.fontName = "Nexa Bold"
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            moneyLabel.fontSize *= 2
+            moneyLabel.position = CGPoint(x: scene.frame.width / 2 - moneyLabel.frame.width / 2 - 10, y: (scene.frame.height + scene.frame.width) / 4 + scene.frame.height / 32)
+        }
+        else {
+            moneyLabel.position = CGPoint(x: scene.frame.width / 2 - moneyLabel.frame.width / 2 - 10, y: (scene.frame.height + scene.frame.width) / 4 + scene.frame.height / 16)
+        }
+        scene.addChild(moneyLabel)
+        
+        upgradeButton.position = CGPoint(x: 0, y: 0)
+        upgradeButton.size = CGSize(width: scene.frame.width / 2, height: scene.frame.width / 2)
+        scene.addChild(upgradeButton)
+    }
+    
+    func getCost() -> Int {
+        return 1000
     }
     
     override var prefersStatusBarHidden: Bool {
